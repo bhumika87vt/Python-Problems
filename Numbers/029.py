6e5b83e5427bb319fca44770250759746be955d7
Bhumika V <bhumikavshetty07@gmail.com>
2026-03-11 18:35:26 +0530

    celsius to fahrenheit and vice-versa

---
+++ Numbers/029.py
@@ -0,0 +1,9 @@
+#Convert Celsius to Fahrenheit
+celsius=25
+fahrenheit=(celsius*9/5)+32
+print(fahrenheit)
+
+#Convert Fahrenheit to Celsius
+fahrenheit=98.6
+celsius=(fahrenheit-32)*5/9
+print(celsius)
